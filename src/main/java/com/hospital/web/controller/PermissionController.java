87c0d2d72a9b252fa69d7807d0b3eb99c758aaf3
yheisun <yheisun@gmail.com>
2017-03-29 17:27:07 +0900

    오후 5:26 2017-03-29 Mapper.xml update

--- src/main/java/com/hospital/web/controller/PermissionController.java
+++ src/main/java/com/hospital/web/controller/PermissionController.java
@@ -1,5 +1,10 @@
 package com.hospital.web.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +14,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
 import com.hospital.web.domain.Info;
 import com.hospital.web.domain.Patient;
 import com.hospital.web.domain.Person;
+import com.hospital.web.domain.Schema;
 import com.hospital.web.mapper.Mapper;
 import com.hospital.web.service.CRUD;
 import com.hospital.web.util.Util;
 
 @Controller
+@SessionAttributes("permission")
 public class PermissionController {
 	private static final Logger logger = LoggerFactory.getLogger(PermissionController.class);
 	@Autowired
@@ -27,10 +36,11 @@ public class PermissionController {
 		return "public:common/loginForm";
 	}
 	@RequestMapping(value = "/{permission}/login", method = RequestMethod.POST)
-	public String login(@RequestParam("id") String id, @RequestParam("password") String pw, @PathVariable String permission,Model model) throws Exception {
+	public String login(@RequestParam("id") String id, @RequestParam("password") String pw, @PathVariable String permission, HttpSession session, Model model) throws Exception {
 		logger.info("PermissionController login() {}", "POST");
 		logger.info("PermissionController login() id,pw: {}", id + pw);
-		logger.info("PermissionController login() group: {}", permission);
+		logger.info("PermissionController login() group: {}", permission); 
+		
 		Person<?> person = new Person<Info>(new Patient());
 		Patient patient = (Patient) person.getInfo();
 		patient.setId(id);
@@ -38,11 +48,16 @@ public class PermissionController {
 		String movePosition = "";
 		switch (permission) {
 		case "patient":
+			Map<String, String> map= new HashMap<>();
+			map.put("group", patient.getGroup());
+			map.put("key", Schema.PATIENT.getName());
+			map.put("value", id);
+			logger.info("key, group, value {}", map.get("key")+map.get("group")+map.get("value"));
 			CRUD.Service ex = new CRUD.Service() {
 				@Override
 				public Object execute(Object o) throws Exception {
 					logger.info("--------------ID ?  {} ---------", o);
-					return mapper.exist(id);
+					return mapper.exist(map);
 				}
 			};
 			Integer count = (Integer) ex.execute(id);
@@ -54,13 +69,14 @@ public class PermissionController {
 				logger.info("DB RESULT: {}", "ID exist");
 				CRUD.Service login = new CRUD.Service() {
 					@Override
-					public Object execute(Object o) throws Exception {
-						return mapper.selectById(id);
+					public Object execute(Object o) throws Exception {	
+						return mapper.findPatient(map);
 					}
 				};
 				patient = (Patient) login.execute(patient);
 				if (patient.getPass().equals(pw)) {
 					logger.info("DB RESULT: {}", "Success");
+					session.setAttribute("permission", patient);
 					String[] getInfo = Util.getInfo(patient.getJumin());
 					model.addAttribute("patient", patient);
 					model.addAttribute("name", patient.getName());
@@ -89,5 +105,10 @@ public class PermissionController {
 			break;
 		}
 		return movePosition;
+	}
+	@RequestMapping("/logout")
+	public String logout(HttpSession session){
+		session.invalidate();
+		return "redirect:/";
 	}
 }
--- src/main/java/com/hospital/web/mapper/Mapper.java
+++ src/main/java/com/hospital/web/mapper/Mapper.java
@@ -1,18 +1,27 @@
 package com.hospital.web.mapper;
 
-import java.sql.SQLException;
-import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Repository;
-
+import com.hospital.web.domain.Doctor;
+import com.hospital.web.domain.Nurse;
 import com.hospital.web.domain.Patient;
 
 @Repository
 public interface Mapper {
-	public int insert(Patient member)throws SQLException;
-	public Patient selectById(String uid)throws SQLException;
-	public boolean login(Patient member)throws SQLException;
-	public int update(Patient[] member)throws SQLException;
-	public int delete(Patient member)throws SQLException;
-	public int count();
-	public int exist(String id) throws Exception;
+	public int registPatient(Patient patient)throws Exception;
+	public int registDoctor(Doctor doctor)throws Exception;
+	public int registNurse(Nurse nurse)throws Exception;
+	public Patient findPatient(Map<?,?> map)throws Exception;
+	public Doctor findDoctor(Map<?,?> map)throws Exception;
+	public Nurse findNurse(Map<?,?> map)throws Exception;
+	public List<Patient> findPatients(Map<?,?> map)throws Exception;
+	public List<Doctor> findDoctors(Map<?,?> map)throws Exception;
+	public List<Nurse> findNurses(Map<?,?> map)throws Exception;
+	public int updatePatient(Patient patient)throws Exception;
+	public int updateDoctor(Doctor doctor)throws Exception;
+	public int updateNurse(Nurse nurse)throws Exception;
+	public int delete(Map<?,?> map)throws Exception;
+	public int count(Map<?,?> map)throws Exception;
+	public int exist(Map<?,?> map)throws Exception;
 }
